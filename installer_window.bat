1d29420b90f4f728f311ad08d9f7d421b52d724e
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-24 15:29:23 +0200

    Added Automatic Installation for Window

--- installer_window.bat
+++ installer_window.bat
@@ -1,6 +1,13 @@
 @echo off
-call .venv/Scripts/activate
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python is not installed.
+    exit /b
+)
 @echo on
+python -m venv .venv
+call .venv\Scripts\activate
+pip install -r requirements.txt
 pyinstaller --noconfirm --onefile --icon "evealert/img/eve.ico" "main.py"
 xcopy /E /I "evealert/img" "dist/evealert/img"
 xcopy /E /I "evealert/sound" "dist/evealert/sound"
