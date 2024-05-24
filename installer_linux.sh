7820088aff6e17226cdf77196204fb9647f3b187
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-24 15:31:16 +0200

    Automatic Installer for Linux

--- installer_linux.sh
+++ installer_linux.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+if ! command -v python &> /dev/null
+then
+    echo "Python is not installed."
+    exit
+fi
+python -m venv .venv
 source .venv/bin/activate
+pip install -r requirements.txt
 pyinstaller --noconfirm --onefile --icon "evealert/img/eve.ico" "main.py"
 cp -r "evealert/img" "dist/evealert/img"
 cp -r "evealert/sound" "dist/evealert/sound"
