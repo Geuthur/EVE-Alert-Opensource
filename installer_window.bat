e7b1ded04a941aabc7c59842e48fbe82b4d6ff92
Geuthur <geuthur@users.noreply.github.com>
2024-10-18 08:57:22 +0200

    [FIX] Windows Execute as Admin not working correctly

--- installer_window.bat
+++ installer_window.bat
@@ -1,7 +1,9 @@
 @echo off
+cd /d "%~dp0"
 where python >nul 2>nul
 if %errorlevel% neq 0 (
     echo Python is not installed.
+    pause
     exit /b
 )
 @echo on
@@ -12,3 +14,4 @@ pyinstaller --noconfirm --onefile --icon "evealert/img/eve.ico" "main.py"
 xcopy /E /I "evealert/img" "dist/evealert/img"
 xcopy /E /I "evealert/sound" "dist/evealert/sound"
 rd /S /Q build
+pause
