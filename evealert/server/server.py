c955ee54c2a47fd2d95366481d9222d2a3a54a6e
Geuthur <geuthur@users.noreply.github.com>
2025-01-02 13:50:35 +0100

    [CHANGE] Moved Alerter System to own Class [ADD] Alert Mute, Server Name [FIX] Some Issues with connection, disconnection, Alerter Authentification

--- evealert/server/server.py
+++ evealert/server/server.py
@@ -1,13 +1,14 @@
 # server.py
+import logging
 import socket
 import threading
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-ADMIN_PASSWORD = "test123"
-
 if TYPE_CHECKING:
     from evealert.menu.main import MainMenu
+
+logger = logging.getLogger("main")
 
 
 class ServerAgent:
@@ -17,6 +18,10 @@ class ServerAgent:
         self.client = None
         self.running = False
         self.stop_event = threading.Event()
+
+        # Default settings
+        self.name = "Eve Local Server"
+        self.admin_password = "1234"
 
     @property
     def is_running(self):
@@ -30,15 +35,15 @@ class ServerAgent:
             if self.server:
                 self.server.close()
                 self.server = None
+                self.main.write_message("Server stopped", "red")
+                Server.log_message("Server stopped")
             if self.client:
-                self.client.close()
+                self.client.clean_up()
                 self.client = None
             self.running = False
             # Close all active connections
             for connection in Server.connections:
                 connection.close()
-            self.main.write_message("Server stopped", "red")
-            Server.log_message("Server stopped")
             Server.connections.clear()
         self.main.mainmenu_buttons.socket_server.configure(
             fg_color="#1f538d", hover_color="#14375e"
@@ -49,8 +54,11 @@ class ServerAgent:
         settings = self.main.menu.setting.load_settings()
         host = settings["server"]["host"]
         port = settings["server"]["port"]
-        admin_password = settings["server"]["admin_password"]
         server = settings["server"]["server_mode"]
+
+        # Set the server name and admin password
+        self.admin_password = settings["server"]["admin_password"]
+        self.name = settings["server"]["name"]
 
         if server:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,27 +76,19 @@ class ServerAgent:
 
             self.main.write_message("Socket Server started successfully", "green")
         else:
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                self.client.connect((host, port))
-                self.client.sendall(admin_password.encode("utf-8"))
-                response = self.client.recv(1024).decode("utf-8")
-                if response == "Alerter access granted!":
-                    self.running = True
-                    # Server.log_message("Connected to server as alerter.")
-                    self.main.write_message("Connected to server as alerter", "green")
+            self.client = ClientAgent(self.main, host, port, self.admin_password)
+            if self.client.connect():
+                self.running = True
+                login = self.client.login(self.admin_password)
+                if login:
+                    self.main.write_message("Alerter successfully logged in!", "green")
+                    Server.log_message("Alerter successfully logged in!")
                     return
-                # Server.log_message("Failed to authenticate with server.")
-                self.main.write_message("Failed to authenticate with server", "red")
-            except OSError as e:
-                if "WinError 10061" in str(e):
-                    # Server.log_message("Failed to connect to server: Connection refused")
-                    self.main.write_message(
-                        "Failed to connect to server: Connection refused", "red"
-                    )
-                else:
-                    # Server.log_message(f"Connection error: {e}")
-                    self.main.write_message(f"Connection error: {e}", "red")
+                self.main.write_message("Wrong Password, Access not granted!", "red")
+                Server.log_message("Wrong Password, Access not granted!")
+            else:
+                self.main.write_message("Client failed to connect", "red")
+                Server.log_message("Client failed to connect")
             self.clean_up()
 
     def newconnections(self):
@@ -99,8 +99,9 @@ class ServerAgent:
                     sock,
                     address,
                     Server.total_connections,
-                    "Name",
+                    self.name,
                     True,
+                    self.admin_password,
                     self.stop_event,
                 )
                 Server.add_connection(new_client)
@@ -121,7 +122,7 @@ class ServerAgent:
                 )
         elif self.client:
             try:
-                self.send_message_to_server(message)
+                self.client.broadcast_message(message)
             except OSError as e:
                 if "WinError 10054" in str(e):
                     self.clean_up()
@@ -129,23 +130,22 @@ class ServerAgent:
                     return
                 Server.log_message(f"Server Send: Failed to broadcast message: {e}")
 
-    def send_message_to_server(self, message: str):
-        """Send a message to the server."""
-        self.client.sendall(message.encode("utf-8"))
-
 
 class Server(threading.Thread):
     connections = []
     total_connections = 0
 
     # pylint: disable=too-many-positional-arguments
-    def __init__(self, st: socket.socket, address, sid, name, signal, stop_event):
+    def __init__(
+        self, st: socket.socket, address, sid, name, signal, password, stop_event
+    ):
         threading.Thread.__init__(self)
         self.socket = st
         self.address = address
         self.id = sid
         self.name = name
         self.signal = signal
+        self.password = password
         self.stop_event = stop_event
 
         # Alerter
@@ -157,14 +157,17 @@ class Server(threading.Thread):
 
     @classmethod
     def add_connection(cls, client):
+        """Add a connection to the list."""
         cls.connections.append(client)
         cls.total_connections += 1
         Server.log_message(
-            f"New connection at ID {client.id}, {client.address} - Admin: {client.is_admin} - Total: {client.total_connections}"
+            f"New connection at ID {client.id}, {client.address} - Total: {client.total_connections}"
         )
+        client.send_message(f"Connetected to {client.name} Server")
 
     @classmethod
     def remove_connection(cls, client):
+        """Remove a connection from the list."""
         if client in cls.connections:
             cls.connections.remove(client)
             cls.total_connections -= 1
@@ -172,11 +175,13 @@ class Server(threading.Thread):
 
     @staticmethod
     def log_message(message):
+        """Log a message to the console."""
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"[{timestamp}] {message}")
 
     @classmethod
     def broadcast_message(cls, message):
+        """Broadcast a message to all connected clients."""
         for client in cls.connections:
             try:
                 client.socket.sendall(message.encode("utf-8"))
@@ -203,9 +208,9 @@ class Server(threading.Thread):
     def run(self):
         while self.signal and not self.stop_event.is_set():
             try:
-                data = self.socket.recv(32)
+                data = self.socket.recv(32).decode("utf-8")
                 if data:
-                    self.handle_message(data.decode("utf-8"))
+                    self.handle_message(data)
             except OSError:
                 self.handle_disconnect()
             except Exception as e:
@@ -217,9 +222,10 @@ class Server(threading.Thread):
         """Handle the message received from the client."""
         try:
             Server.log_message(f"Received from {self.address}: {message}")
-            if message == ADMIN_PASSWORD:
-                self.socket.sendall(b"Alerter access granted!")
+            if message == self.password:
                 self.is_admin = True
+                self.socket.sendall(b"Alerter access granted!")
+                self.socket.sendto(b"Alerter access granted!", self.address)
                 Server.broadcast_message("Alerter has loged in!")
             elif message == "Alert":
                 Server.broadcast_message("Alert")
@@ -227,5 +233,99 @@ class Server(threading.Thread):
             elif message == "Normal":
                 Server.broadcast_message("Normal")
                 Server.log_message(f"Normal broadcasted by {self.address}")
+            elif message == "Heartbeat":
+                pass
+            else:
+                self.socket.sendall(b"Invalid Command")
+                self.handle_disconnect()
         except OSError as e:
             print(f"Error: {e}")
+
+
+class ClientAgent:
+    def __init__(self, main: "MainMenu", host, port, password):
+        self.main = main
+        self.port = port
+        self.host = host
+        self.password = password
+
+        self.sock = None
+        self.running = False
+        self.stop_event = threading.Event()
+
+    @property
+    def is_running(self):
+        """Check if the client is running."""
+        return self.running
+
+    def clean_up(self):
+        """Clean up the client."""
+        if self.running:
+            self.stop_event.set()
+            if self.sock:
+                self.sock.close()
+                self.sock = None
+            self.running = False
+            self.main.write_message("Alerter stopped", "red")
+            Server.log_message("Alerter stopped")
+
+    def connect(self):
+        """Connect to the server."""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect((self.host, self.port))
+            response = self.sock.recv(1024).decode("utf-8")
+            if "Connetected" in response:
+                self.main.write_message(response, "green")
+                Server.log_message(response)
+                self.running = True
+                return True
+            return False
+        except OSError as e:
+            if "WinError 10061" in str(e):
+                self.main.write_message(
+                    "Failed to connect to server: Connection refused", "red"
+                )
+            elif "WinError 10053" in str(e):
+                pass
+            else:
+                logger.debug("Connection error: %s", e)
+                self.main.write_message("Failed to connect to server, Read Logs", "red")
+        except Exception as e:
+            logger.exception("An unexpected error occurred: %s", e)
+            self.main.write_message("An unexpected error occurred, Read Logs", "red")
+        self.clean_up()
+        return False
+
+    def login(self, password):
+        """Send login message to the server."""
+        if self.running:
+            try:
+                self.sock.sendall(password.encode("utf-8"))
+                response = self.sock.recv(1024).decode("utf-8")
+                if "Alerter access granted!" in response:
+                    return True
+                return False
+            except OSError as e:
+                self.main.write_message(f"Failed to login: {e}", "red")
+                self.clean_up()
+                return False
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+                self.clean_up()
+                return False
+        return False
+
+    def broadcast_message(self, message):
+        """Broadcast a message to the server."""
+        if self.running:
+            try:
+                self.sock.sendall(message.encode("utf-8"))
+            except OSError as e:
+                self.main.write_message(f"Failed to broadcast message: {e}", "red")
+                self.clean_up()
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
+                self.clean_up()
+        else:
+            self.main.write_message("Alerter not running", "red")
